10a153310fe6429751091458dd73decbdd3d3741
Mihir Parmar <41247825+Mihir7027@users.noreply.github.com>
2026-05-26 00:36:59 +0530

    AI: Update lib/core/router/routes/home_routes.dart

--- lib/core/router/routes/home_routes.dart
+++ lib/core/router/routes/home_routes.dart
@@ -1,9 +1,13 @@
 import 'package:go_router/go_router.dart';
-import '../../../../features/profile/presentation/screens/profile_screen.dart';
-import '../../../../core/router/route_names.dart';
+import '../route_names.dart';
+import '../../features/home/presentation/screens/home_screen.dart';
+import '../../features/profile/presentation/screens/profile_screen.dart';
 
-final List<GoRoute> homeRoutes = <GoRoute>[
-  // Other routes...
+final List<GoRoute> homeRoutes = [
+  GoRoute(
+    path: RouteNames.home,
+    builder: (context, state) => const HomeScreen(),
+  ),
   GoRoute(
     path: RouteNames.profile,
     name: RouteNames.profile,
