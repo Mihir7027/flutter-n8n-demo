4b1b241b7eb4e30a687ee8ca44b648bbc274a80f
Mihir Parmar <41247825+Mihir7027@users.noreply.github.com>
2026-05-26 00:10:02 +0530

    AI: Add lib/features/profile/domain/models/profile_model.dart

---
+++ lib/features/profile/domain/models/profile_model.dart
@@ -0,0 +1,19 @@
+class Profile {
+  final String name;
+  final String email;
+  final String phone;
+  final String gender;
+  final DateTime dateOfBirth;
+  final String location;
+  final String profession;
+
+  Profile({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.gender,
+    required this.dateOfBirth,
+    required this.location,
+    required this.profession,
+  });
+}
