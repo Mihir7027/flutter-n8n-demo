b94b934c9ad87f107acb85dd78922310d9591507
Mihir Parmar <41247825+Mihir7027@users.noreply.github.com>
2026-05-26 00:23:23 +0530

    AI: Update lib/core/router/routes/home_routes.dart

--- lib/core/router/routes/home_routes.dart
+++ lib/core/router/routes/home_routes.dart
@@ -1,20 +1,9 @@
 import 'package:go_router/go_router.dart';
-import '../../../features/home/presentation/screens/home_screen.dart';
-import '../../../features/calculator/presentation/screens/calculator_screen.dart';
-import '../../../features/profile/presentation/screens/profile_screen.dart';
-import '../route_names.dart';
+import '../../../../features/profile/presentation/screens/profile_screen.dart';
+import '../../../../core/router/route_names.dart';
 
-final homeRoutes = <RouteBase>[
-  GoRoute(
-    path: RouteNames.home,
-    name: RouteNames.home,
-    builder: (context, state) => const HomeScreen(),
-  ),
-  GoRoute(
-    path: RouteNames.calculator,
-    name: RouteNames.calculator,
-    builder: (context, state) => const CalculatorScreen(),
-  ),
+final List<GoRoute> homeRoutes = <GoRoute>[
+  // Other routes...
   GoRoute(
     path: RouteNames.profile,
     name: RouteNames.profile,
